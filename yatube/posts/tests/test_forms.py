6f0f1ca8c57351562c5bd5c03bc936eccb4247db
Elena Smirnova <ev_smir@mail.ru>
2022-12-01 19:13:41 +0300

    Fourth corrections

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -38,10 +38,6 @@ class PostsFormsTest(TestCase):
         cls.POST_DETAIL_URL = reverse(
             'posts:post_detail',
             kwargs={'post_id': cls.post.id})
-        cls.FORM_DATA = {
-            'text': 'Тестовый пост формы',
-            'group': cls.group.id,
-        }
 
     def setUp(self):
         self.guest_client = Client()
@@ -50,39 +46,28 @@ class PostsFormsTest(TestCase):
 
     def test_posts_forms_create_post(self):
         """Проверка, создает ли форма пост в базе."""
-        posts_before = self.authorized_client.get(
-            PROFILE_URL).context['page_obj']
+        FORM_DATA = {
+            'text': 'Тестовый пост формы',
+            'group': self.group.id}
+        posts_before = Post.objects.all()
         posts_before_count = len(posts_before)
-
         response = self.authorized_client.post(
             POST_CREATE_URL,
-            data=self.FORM_DATA,
-        )
-        self.assertRedirects(
-            response,
-            PROFILE_URL
-        )
-
-        posts_after = self.authorized_client.get(
-            PROFILE_URL).context['page_obj']
-        old_posts_count = 0
+            data=FORM_DATA)
+        self.assertRedirects(response, PROFILE_URL)
+        posts_after = Post.objects.all()
+        posts_after_count = len(posts_after)
         for post in posts_after:
-            if post in posts_before:
-                old_posts_count += 1
-            else:
+            if not (post in posts_before):
                 new_post = post
-
-        self.assertEqual(old_posts_count, posts_before_count)
-        self.assertEqual(new_post.text, self.FORM_DATA['text'])
+        self.assertEqual(posts_after_count, posts_before_count + 1)
+        self.assertEqual(new_post.text, FORM_DATA['text'])
         self.assertEqual(new_post.author, self.author)
-        self.assertEqual(new_post.group_id, self.FORM_DATA['group'])
+        self.assertEqual(new_post.group_id, FORM_DATA['group'])
 
     def test_authorized_user_edit_post(self):
         """Проверка редактирования записи авторизированным клиентом."""
-        posts_before = self.authorized_client.get(
-            PROFILE_URL).context['page_obj']
-        posts_before_count = len(posts_before)
-
+        posts_before = Post.objects
         form_data = {
             'text': 'Отредактированный текст поста',
             'group': self.group2.id,
@@ -95,42 +80,28 @@ class PostsFormsTest(TestCase):
             response,
             self.POST_DETAIL_URL
         )
-
-        posts_after = self.authorized_client.get(
-            PROFILE_URL).context['page_obj']
-        old_posts_count = 0
-        for post in posts_after:
-            ind = posts_before.index(post)
-            if (posts_before[ind].text == post.text
-                    and posts_before[ind].group_id == post.group_id):
-                old_posts_count += 1
-            else:
-                edit_post = post
-                post_before = posts_before[ind]
-
-        self.assertEqual(old_posts_count + 1, posts_before_count)
+        posts_after = Post.objects
+        edit_post = posts_after.get(pk=self.post.pk)
+        self.assertEqual(posts_after.count(), posts_before.count())
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
         self.assertEqual(edit_post.text, form_data['text'])
-        self.assertEqual(edit_post.author, post_before.author)
+        self.assertEqual(edit_post.author, self.post.author)
         self.assertEqual(edit_post.group_id, form_data['group'])
 
     def test_nonauthorized_user_create_post(self):
         """Проверка создания записи не авторизированным пользователем."""
-        posts_before = self.guest_client.get(INDEX_URL).context['page_obj']
-        posts_before_count = len(posts_before)
-
+        FORM_DATA = {
+            'text': 'Тестовый пост формы',
+            'group': self.group.id}
+        posts_before = Post.objects
+        posts_before_count = posts_before.count()
         response = self.guest_client.post(
             POST_CREATE_URL,
-            data=self.FORM_DATA,
-        )
-
-        posts_after = self.guest_client.get(INDEX_URL).context['page_obj']
-        old_posts_count = 0
-        for post in posts_after:
-            if post in posts_before:
-                old_posts_count += 1
-
+            data=FORM_DATA)
+        posts_after = Post.objects
+        posts_after_count = Post.objects.count()
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
         redirect = LOGIN_URL + '?next=' + POST_CREATE_URL
         self.assertRedirects(response, redirect)
-        self.assertEqual(old_posts_count, posts_before_count)
+        self.assertEqual(posts_after_count, posts_before_count)
+        self.assertEqual(posts_before, posts_after)
