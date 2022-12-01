ac5a624c859e5af92bc34a162795ad14c425ead2
Elena Smirnova <ev_smir@mail.ru>
2022-12-01 19:16:08 +0300

    Fourth corrections

--- yatube/posts/constants.py
+++ yatube/posts/constants.py
@@ -5,5 +5,5 @@ INDEX_TEMPLATE = 'posts/index.html'
 GROUP_LIST_TEMPLATE = 'posts/group_list.html'
 PROFILE_TEMPLATE = 'posts/profile.html'
 POST_DETAIL_TEMPLATE = 'posts/post_detail.html'
-
+POST_EDIT_TEMPLATE = 'posts/create_post.html'
 CREATE_POST_TEMPLATE = 'posts/create_post.html'
